6f282931e860e0d43da1a40dad4d60b2016e7531
Nicolas Masselot <nico@MacBook-Air-de-Nicolas-3.local>
2025-09-27 12:33:52 +0200

    Mon message de commit

---
+++ BSM project/README.md
@@ -0,0 +1,6 @@
+Src : la source
+Notebooks : résultats visuels
+tests : le contrôle
+.md : format de texte
+=> readme  à mettre en forme avant publication
+Jupyter
---
+++ BSM project/src/pricing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy.stats as si
+
+def call_price(S0, K, T, r, sigma):
+    d1 = (np.log(S0/K) + (r + 0.5*sigma**2)*T)/(sigma/np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    return S0*si.norm.cdf(d1,0,1) - K*np.exp(-r*T)*si.norm.cdf(d2,0,1)
+        
+def put_price(S0, K, T, r, sigma):
+    d1 = (np.log(S0/K) + (r + 0.5*sigma**2)*T)/(sigma/np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    return (K*np.exp(-r*T)*si.norm.cdf(-d2,0,1) - S0*si.norm.cdf(-d1,0,1))
+   
